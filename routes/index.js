ebdb959c2061d28fc8ced65d4777070c70f3adb8
Maxime Golfier <maxgolfier@gmail.com>
2017-11-22 16:40:09 +0100

    Addview and display viewi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/display_actor', function(req, res, next) {
+  res.render('display_actor');
+});
+
+router.get('/register_actor', function(req, res, next) {
+  res.render('register_actor');
+});
+
 
 module.exports = router;
