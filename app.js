fc532c23525f350c0ea1cff834ee9c4723e769c1
Maxime Golfier <maxgolfier@gmail.com>
2017-11-23 19:19:31 +0100

    i

--- app.js
+++ app.js
@@ -10,6 +10,47 @@ var users = require('./routes/users');
 
 var app = express();
 
+var http = require('http');
+var cfenv = require("cfenv");
+
+/***** Cloudant part *****/
+var vcapLocal; // load local VCAP configuration  and service credentials
+try {
+  vcapLocal = require('./vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
+  // Load the Cloudant library.
+  var Cloudant = require('cloudant');
+
+  // Initialize database with credentials
+  if (appEnv.services['cloudantNoSQLDB']) {
+     // CF service named 'cloudantNoSQLDB'
+     var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  } else {
+     // user-provided service with 'cloudant' in its name
+     var cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+  }
+
+  //database name
+  var dbName = 'mydb';
+
+  // Create a new "mydb" database.
+  cloudant.db.create(dbName, function(err, data) {
+    if(!err) //err if database doesn't already exists
+      console.log("Created database: " + dbName);
+  });
+
+  // Specify the database we are going to use (mydb)...
+  mydb = cloudant.db.use(dbName);
+}
+/***** Cloudant part *****/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mydb;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,5 +15,20 @@ router.get('/register_actor', function(req, res, next) {
   res.render('register_actor');
 });
 
+router.post('/register_actor', function(req, res, next) {
+  var userName = request.body.name;
+  var id = request.body.id;
+  if(!mydb) {
+    console.log("No database.");
+    //response.send("No database!");
+    return;
+  }
+  mydb.insert({ "name" : userName, "id" : id }, function(err, body, header) {
+    if (err) {
+      return console.log('[mydb.insert] ', err.message);
+    }
+    //response.send("New entry in the database.");
+  });
+});
 
 module.exports = router;
