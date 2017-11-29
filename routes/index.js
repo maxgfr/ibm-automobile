d00ac1d40aacd8b1a7d043cba7dadbdf4187788a
Maxime Golfier <maxgolfier@gmail.com>
2017-11-29 14:46:09 +0100

    it works

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var mydb;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,23 +10,41 @@ router.get('/display_actor', function(req, res, next) {
   res.render('display_actor');
 });
 
+router.get('/get_actor', function(req, res, next) {
+    var data = [];
+    if(!mydb) {
+        res.json(data);
+        return;
+    }
+
+  mydb.list({ include_docs: true }, function(err, body) {
+    if (!err) {
+        body.rows.forEach(function(row) {
+            if(row.doc.name && row.doc.id)
+                data.push({id: row.doc.id, name: row.doc.name});
+        });
+      res.json(data);
+    }
+  });
+});
+
 router.get('/register_actor', function(req, res, next) {
   res.render('register_actor');
 });
 
 router.post('/register_actor', function(req, res, next) {
-  var userName = request.body.name;
-  var id = request.body.id;
+  var userName = req.body.name;
+  var id = req.body.id;
+  //console.log(mydb);
   if(!mydb) {
-    console.log("No database.");
-    //response.send("No database!");
+    res.send("Pas de database...");
     return;
   }
   mydb.insert({ "name" : userName, "id" : id }, function(err, body, header) {
     if (err) {
-      return console.log('[mydb.insert] ', err.message);
+      return null; res.send('[mydb.insert] ', err.message);
     }
-    //response.send("New entry in the database.");
+    res.send("Nouvelle entrée dans la base de donnée :)");
   });
 });
 
